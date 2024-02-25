c9dfc4dd6d7ab5e27d367142365a663c746c5ede
Davide64-dev <davidpcu@hotmail.com>
2024-02-25 19:13:38 +0000

    added main file

--- board.py
+++ board.py
@@ -1,4 +1,25 @@
+import random
+
+
 class Board:
 
     def __init__(self, board_size):
-        self.board = [[0 for _ in range(board_size)] for _ in range(board_size)]
+        self.board = [[False for _ in range(board_size)] for _ in range(board_size)]
+        
+
+        list = []
+        count = 0
+        while count < 9:
+            x = random.randint(0, board_size - 1)
+            y = random.randint(0, board_size - 1)
+            if (x, y) in list:
+                continue
+            else:
+                self.board[x][y] = True
+                count += 1
+                list.append((x,y))
+
+    def __str__(self):
+        for row in self.board:
+            print(row)
+
---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+import pygame_menu
+
+def main():
+
+    print("Game started")
+
+if __name__ == "__main__":
+    main()
