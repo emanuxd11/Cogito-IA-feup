bf15e7bf2be911dbd70a0dcd36a56e55f1010d66
Davide64-dev <davidpcu@hotmail.com>
2024-03-28 11:38:01 +0000

    working but entering infinite cycle idk why

--- src/algorithms/a_star.py
+++ src/algorithms/a_star.py
@@ -8,37 +8,38 @@ class AStar(Bot):
     def __init__(self, game, heuristic_func):
         super().__init__(game)
         self.heuristic_func = heuristic_func
-        self.came_from = []
+        self.came_from = {}
 
     def make_move(self):
 
         if self.game.is_moving or not self.game.level_active:
             return
-        
-        if self.came_from == []:
+
+        move_sequence = []
+        if not self.came_from:
             self.came_from, _ = self.a_star()
-            self.came_from.reverse()
-        
+            print(self.came_from)
+            current_board = self.game
+            while not self.game.board.board == current_board.board.objective:
+                print(current_board.board)
+                next_board, move = self.came_from[current_board.board]
+                current_board = next_board
+                move_sequence.append(move)
+            move_sequence.reverse()
+
         def move_caller():
             self.game.is_moving = True
             pygame.time.delay(10)
 
-            row, col = self.came_from[0]
-            self.game.make_move(row, col)
-            self.came_from.pop(0)
+            if move_sequence:
+                row, col = move_sequence.pop(0)
+                self.game.make_move(row, col)
+            else:
+                print("[LOG] No more moves in sequence")
+
             Sound.playMoveSound()
             self.game.move_count += 1
             self.game.is_moving = False
-
-            print(f"[LOG] Bot Marley made move on row -1 and column -1 moves:" + str(len(self.came_from)))
-            print(f"[LOG] Bot Marley made move on row -1 and column -1 moves:" + str(len(self.came_from)))
-            print(f"[LOG] Bot Marley made move on row -1 and column -1 moves:" + str(len(self.came_from)))
-            print(f"[LOG] Bot Marley made move on row -1 and column -1 moves:" + str(len(self.came_from)))
-            print(f"[LOG] Bot Marley made move on row -1 and column -1 moves:" + str(len(self.came_from)))
-            print(f"[LOG] Bot Marley made move on row -1 and column -1 moves:" + str(len(self.came_from)))
-            print(f"[LOG] Bot Marley made move on row -1 and column -1 moves:" + str(len(self.came_from)))
-            print(f"[LOG] Bot Marley made move on row -1 and column -1 moves:" + str(len(self.came_from)))
-            print(f"[LOG] Bot Marley made move on row -1 and column -1 moves:" + str(len(self.came_from)))
 
         move_thread = threading.Thread(target=move_caller)
         move_thread.start()
@@ -46,11 +47,11 @@ class AStar(Bot):
     def a_star(self):
         frontier = PriorityQueue()
         frontier.put((0, self.game))
-        came_from = []
+        came_from = {}
         cost_so_far = {self.game: 0}
 
         while not frontier.empty():
-            print(int(frontier.qsize()))
+            print(int(frontier.qsize()))  # Track queue size for debugging
             _, current = frontier.get()
 
             print(current.board)
@@ -63,15 +64,14 @@ class AStar(Bot):
                 node_game, move = next_node
                 new_cost = cost_so_far[current] + 1
 
-                if (node_game.board == current.board):
-                    new_cost = float('inf')
+                if node_game.board == current.board:
+                    new_cost = float('inf')  # Penalize revisiting same state
 
                 if node_game not in cost_so_far or new_cost < cost_so_far[node_game]:
                     cost_so_far[node_game] = new_cost
                     priority = new_cost + self.heuristic_func(node_game.board)
                     frontier.put((priority, node_game))
-                    #came_from[node_game.board] = move
-                    came_from.append(move)
+                    came_from[node_game.board] = (current, move)
 
         return came_from, cost_so_far
 
