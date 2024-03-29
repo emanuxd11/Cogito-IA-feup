57e709d5f0a9766e14e1ca32f7d4baacb88f63c9
Davide64-dev <davidpcu@hotmail.com>
2024-03-29 22:35:57 +0000

    solved a*

--- src/algorithms/a_star.py
+++ src/algorithms/a_star.py
@@ -5,12 +5,22 @@ from queue import PriorityQueue
 import threading
 from board import Board
 
+class Node:
+    def __init__(self, game, parent=None, move=None):
+        self.game = game
+        self.parent = parent
+        self.move = move
+
+    def __lt__(self, other):
+        return self.game.board < other.game.board
+
 class AStar(Bot):
     def __init__(self, game, heuristic_func):
         super().__init__(game)
         self.heuristic_func = heuristic_func
         self.came_from = {}
         self.move_sequence = []
+        self.tree = []
 
     def make_move(self):
 
@@ -18,7 +28,9 @@ class AStar(Bot):
             return
 
         if not self.came_from:
-            self.came_from, _ = self.a_star()
+            self.move_sequence = self.a_star()
+            print(self.move_sequence)
+            """
             current_board = Board(9)
             current_board.board = self.gamecopy.board.objective.copy()
             while self.gamecopy.board.board != current_board.board:
@@ -33,10 +45,11 @@ class AStar(Bot):
                 current_board = temp
                 self.move_sequence.append(move)
             self.move_sequence.reverse()
+            """
         
         def move_caller():
             self.game.is_moving = True
-            pygame.time.delay(10)
+            pygame.time.delay(100)
             print(self.move_sequence)
             if self.move_sequence:
                 row, col = self.move_sequence[0]
@@ -55,38 +68,44 @@ class AStar(Bot):
         
     def a_star(self):
         frontier = PriorityQueue()
-        frontier.put((0, self.game))
-        came_from = {}
-        cost_so_far = {self.game: 0}
+        initial_node = Node(self.game)
+        self.tree.append(initial_node)
+        frontier.put((0, initial_node))
+        cost_so_far = {initial_node.game.board: 0}
 
         while not frontier.empty():
             print(int(frontier.qsize()))  # Track queue size for debugging
             _, current = frontier.get()
 
-            print(current.board)
+            print(current.game.board)
 
-            if current.board.isWinningBoard():
+            if current.game.board.isWinningBoard():
+                return self.construct_path(current)
                 break
 
-            valid_moves = current.valid_moves()
+            valid_moves = current.game.valid_moves()
             for next_node in valid_moves:
                 node_game, move = next_node
-                new_cost = cost_so_far[current] + 1
+                new_cost = cost_so_far[current.game.board] + 1
 
-                if node_game.board == current.board:
+                if node_game.board == current.game.board:
                     new_cost = float('inf')  # Penalize revisiting same state
                     
 
-                if node_game not in cost_so_far or new_cost < cost_so_far[node_game]:
-                    cost_so_far[node_game] = new_cost
+                if node_game.board not in cost_so_far or new_cost < cost_so_far[node_game.board]:
+                    cost_so_far[node_game.board] = new_cost
                     priority = new_cost + self.heuristic_func(node_game.board)
-                    frontier.put((priority, node_game))
-                    came_from[node_game.board] = (current, move)
+                    new_node = Node(node_game, current, move)
+                    frontier.put((priority, new_node))
+                    self.tree.append(new_node)
+                    #came_from[node_game.board] = (current, move)
 
-        return came_from, cost_so_far
+        return new_node
 
-    def find_difference(self, board1, board2):
-        for row in range(len(board1)):
-            for col in range(len(board1)):
-                if board1[row][col] != board2[row][col]:
-                    return row, col
+    def construct_path(self, node):
+        path = []
+        while node.parent:
+            path.append(node.move)
+            node = node.parent
+        path.reverse()
+        return path
