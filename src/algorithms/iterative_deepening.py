735070df227570bc2d5c20142a6819f1c071d380
Bernardo <6ernardocampos@gmail.com>
2024-03-29 15:51:32 +0000

    IDS complete. Not finding solution in time

--- src/algorithms/iterative_deepening.py
+++ src/algorithms/iterative_deepening.py
@@ -1,7 +1,7 @@
 from bot import Bot
 import pygame
 from sound import Sound
-from collections import deque
+import threading
 
 class Node:
     def __init__(self, game, depth, parent=None, move=None):
@@ -11,41 +11,35 @@ class Node:
         self.move = move
 
 class IterativeDeepening(Bot):
-    def __init__(self, game):
+    def __init__(self, game, move_list=[]):
         super().__init__(game)
-        self.board_sequence = []
+        self.move_list = move_list
 
     def make_move(self):
         if self.game.is_moving or not self.game.level_active:
             return
+        
+        if self.move_list == []:
+            self.move_list = self.iterative_deepening()
 
-        def move_caller(newGame):
+        def move_caller():
             self.game.is_moving = True
             pygame.time.delay(100)
-            self.game = newGame
+
+            print(self.move_list[0])
+
+            row, col = self.move_list[0]
+            self.game.make_move(row, col)
+            self.move_list.pop(0)
             Sound.playMoveSound()
             self.game.move_count += 1
             self.game.is_moving = False
-            # print(f"[LOG] Bot Marley made move on row {row} and column {col}")
+            
+            print(f"[LOG] Bot Marley made move on row {row} and column {col}")
 
-        came_from = self.iterative_deepening()
 
-        print(came_from)
-
-        current = self.game
-
-        #ajustar isto
-
-        while current.board.board != self.game.board.objective:
-            self.board_sequence.append(current.board)
-            current = came_from[current]
-        
-        self.board_sequence.append(current.board)
-
-        self.board_sequence.reverse()
-
-        for game in self.board_sequence:
-            move_caller(game)
+        move_thread = threading.Thread(target=move_caller)
+        move_thread.start()
             
     def iterative_deepening(self):
         max_depth = 0
@@ -53,6 +47,7 @@ class IterativeDeepening(Bot):
         while True:
             solution = self.dfs(max_depth)
             if solution:
+                print(solution)
                 return solution 
             
             max_depth += 1
